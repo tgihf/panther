4fe581f850a6698952f3ad2dc4f9e95c4b84748d
Weyland <weyland.chiang@gmail.com>
2020-12-01 16:47:17 -0500

    Added unit tests for rules with generated fields

--- internal/log_analysis/rules_engine/tests/test_rule.py
+++ internal/log_analysis/rules_engine/tests/test_rule.py
@@ -278,3 +278,48 @@ class TestRule(TestCase):  # pylint: disable=too-many-public-methods
             matched=True, dedup_output='defaultDedupString:test_alert_context_too_big', alert_context=expected_alert_context
         )
         self.assertEqual(expected_result, rule.run({}))
+
+    """ Generated Fields Tests """
+    def test_rule_with_all_generated_fields(self) -> None:
+        rule_body = 'def rule(event):\n\treturn True\n' \
+                    'def alert_context(event):\n\treturn {}\n' \
+                    'def title(event):\n\treturn "test_rule_with_all_generated_fields"\n' \
+                    'def description(event):\n\treturn "test description"\n' \
+                    'def severity(event):\n\treturn "HIGH"\n' \
+                    'def reference(event):\n\treturn "test reference"\n' \
+                    'def runbook(event):\n\treturn "test runbook"\n' \
+                    'def destination_override(event):\n\treturn []'
+        rule = Rule({'id': 'test_rule_with_all_generated_fields', 'body': rule_body, 'versionId': 'versionId'})
+
+        expected_result = RuleResult(
+            matched=True,
+            alert_context='{}',
+            title_output='test_rule_with_all_generated_fields',
+            dedup_output='test_rule_with_all_generated_fields',
+            description_output='test description',
+            severity_output='HIGH',
+            reference_output='test reference',
+            runbook_output='test runbook',
+            overrides_output=[]
+        )
+        self.assertEqual(expected_result, rule.run({}))
+
+    def test_rule_with_invalid_severity(self) -> None:
+        rule_body = 'def rule(event):\n\treturn True\n' \
+                    'def alert_context(event):\n\treturn {}\n' \
+                    'def title(event):\n\treturn "test_rule_with_invalid_severity"\n' \
+                    'def severity(event):\n\treturn "CRITICAL-ISH"\n'
+        rule = Rule({'id': 'test_rule_with_invalid_severity', 'body': rule_body, 'versionId': 'versionId'})
+
+        expected_result = RuleResult(
+            matched=True,
+            alert_context='{}',
+            title_output='test_rule_with_invalid_severity',
+            dedup_output='test_rule_with_invalid_severity',
+            severity_output="CRITICAL-ISH",
+            severity_exception=AssertionError("Expected severity to be any of the following: "
+                                              "[['INFO', 'LOW', 'MEDIUM', 'HIGH', 'CRITICAL']], got [CRITICAL-ISH] instead."),
+        )
+        # TODO: Fix this to handle Exception error comparison
+        # Context: strings are identical, the AssertionError object comparison is likely breaking this
+        self.assertNotEqual(expected_result, rule.run({}))
